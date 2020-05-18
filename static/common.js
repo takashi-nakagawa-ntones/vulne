4df65e6ded8a7d4ba8834f32f395e52de93a0b72
root <root@localhost.localdomain>
2020-05-18 19:28:25 +0900

    homebutton link mod

--- static/common.js
+++ static/common.js
@@ -11,7 +11,7 @@
   //Method
 
     function clickHomeIcon(){
-      var form = $("<form>").attr("method","post").attr("name","form1").attr("action","/");
+      var form = $("<form>").attr("method","post").attr("name","form1").attr("action","/vulne/");
       var page = $("<input>").attr("type", "hidden").attr("name", "page").val("index");
       var title = $("<input>").attr("type", "hidden").attr("name", "title").val("トップ");
       $(form).append(page).append(title);
